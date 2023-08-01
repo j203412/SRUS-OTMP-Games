df4b457fb326763caeadff0538547b9379041b1b
MightyMuso <proslmeyr@icloud.com>
2023-08-01 11:06:37 +0800

    player_node.py

--- app/player.py
+++ app/player.py
@@ -4,9 +4,11 @@ class Player:
         self._uid = uid
         self._name = name
 
+    @property
     def uid(self):
         return self._uid
 
+    @property
     def name(self):
         return self._name
 
--- player_node.py
+++ player_node.py
@@ -1,21 +1,41 @@
+from player import Player
+
+
 class PlayerNode:
 
-    def __init__(self, player):
+    def __init__(self, player: Player):
         self._player = player
         self.next_node = None
         self.previous_node = None
 
-    def get_player(self):
+
+    @property
+    def player(self):
         return self._player
 
-    def get_next_node(self):
+    @property
+    def next_node(self):
         return self.next_node
 
-    def get_previous_node(self):
+    @property
+    def previous_node(self):
         return self.previous_node
 
+    @player.setter
+    def player(self, value):
+        self._player = value
+
+    @next_node.setter
+    def next_node(self, value):
+        self.next_node = value
+
+    @previous_node.setter
+    def previous_node(self, value):
+        self.previous_node = value
+
+    @property
     def key(self):
-        return self.player._uid
+        return self._player.uid
 
     def __str__(self):
         return f"{self._player}"
